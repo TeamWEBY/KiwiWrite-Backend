0d2c2747a36a90c2edf683c32e41835eba50dcee
leeseohyun <applemint2003@gmail.com>
2023-07-10 00:37:46 +0900

    프로젝트 생성

---
+++ src/test/java/weby/kiwi/KiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package weby.kiwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

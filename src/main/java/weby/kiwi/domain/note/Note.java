7c1fd9dcca0a908be51e76ac033da1e0bed11d15
Pearl <rkdwlswn227@gmail.com>
2023-08-14 15:08:32 +0900

    Note 도메인 뼈대 생성 작업
    Class 3개 (Note, NoteController, NoteRequest)
    Interface 1개 (NoteRepository)
    
    생성한 후 Note 기능에 대한 기본 코드 세팅

---
+++ src/main/java/weby/kiwi/domain/note/Note.java
@@ -0,0 +1,48 @@
+package weby.kiwi.domain.note;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    // 기본 생성자, Getter, Setter
+    // 해당 Note Entity의 필드에 접근하고 값을 설정하기 위해 사용함
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+

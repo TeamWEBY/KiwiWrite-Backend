f4d8c8bff407bae37b6c05d74dfcc1d2372fd1eb
Pearl <rkdwlswn227@gmail.com>
2023-08-17 15:48:34 +0900

    의존성 주입 코드로 수정
    NoteService 코드를 의존성 주입하는 코드로 수정했음

--- src/main/java/weby/kiwi/note/NoteService.java
+++ src/main/java/weby/kiwi/note/NoteService.java
@@ -8,11 +8,14 @@ import weby.kiwi.user.UserRepository;
 @Service
 public class NoteService {
 
-    @Autowired
-    private NoteRepository noteRepository;
+    private final NoteRepository noteRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    @Autowired // 생성자 주입을 사용, 두 Repository를 주입하는 방식으로 코드 수정
+    public NoteService(NoteRepository noteRepository, UserRepository userRepository) {
+        this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
+    }
 
     public Note createNote(Long userId, String title, String content){
         User user = userRepository.findById(userId)

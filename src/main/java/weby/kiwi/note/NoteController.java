4b2959aba1a01c50ea445d76d4d6597d9cd25ef1
Pearl <rkdwlswn227@gmail.com>
2023-08-17 13:37:28 +0900

    Note 파일 초안 브랜치 옮기기
    main 브랜치에 있던 것을 feature/note 브랜치로 옮기는 작업

---
+++ src/main/java/weby/kiwi/note/NoteController.java
@@ -0,0 +1,32 @@
+package weby.kiwi.note;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// HTTP 리퀘스트를 처리하기 위한 NoteController 클래스 생성
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+    private final NoteRepository noteRepository;
+
+    @Autowired
+    public NoteController(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> createNote(@RequestBody NoteRequest noteRequest) {
+        Note newNote = new Note();
+        newNote.setTitle(noteRequest.getTitle());
+        newNote.setContent(noteRequest.getContent());
+
+        Note savedNote = noteRepository.save(newNote);
+
+        return ResponseEntity.ok(savedNote);
+    }
+}

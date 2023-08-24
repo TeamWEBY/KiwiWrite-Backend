8bc2ad9678761baf0422faeb0d4530ee793a9764
Pearl <rkdwlswn227@gmail.com>
2023-08-24 15:50:04 +0900

    NoteTest 기능 1차
    Word를 제외한 Note 단독 생성 기능 테스트 코드 추가 (DB 연결까지 확인 완료)

--- src/main/java/weby/kiwi/user/User.java
+++ src/main/java/weby/kiwi/user/User.java
@@ -3,18 +3,41 @@ package weby.kiwi.user;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id") // 매핑 추가
     private Long id;
 
-    @Column(nullable = false, length = 50)
+    @Column(name = "name") // 필드명 수정
     private String username;
 
-    @Column
+    @Column(name = "email")
     private String email;
 
-    // Getter, Setter, Constructors, etc.
+    @Column(name = "passwd")
+    private String passwd;
+
+    public void setId(Long id) { // 타입 변경
+        this.id = id;
+    }
+
+    public void setUsername(String username) { // 메소드명 수정
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
 }

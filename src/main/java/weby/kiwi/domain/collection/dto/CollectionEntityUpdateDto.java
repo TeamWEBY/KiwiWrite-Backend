c9571c761851d8788bfb220a5e1fa329ec846aa1
Min Gyung Park <kateking001130@ewhain.net>
2023-08-24 17:24:51 +0900

    update Collection code

--- src/main/java/weby/kiwi/domain/collection/dto/CollectionEntityUpdateDto.java
+++ src/main/java/weby/kiwi/domain/collection/dto/CollectionEntityUpdateDto.java
@@ -6,16 +6,15 @@ import lombok.NoArgsConstructor;
 import weby.kiwi.domain.word.entity.Word;
 
 import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class CollectionEntityUpdateDto {
-    private int year;
     private int month;
     private List<Word> wordList;
 
     @Builder
-    public CollectionEntityUpdateDto(int year, int month, List<Word> wordList) {
-        this.year = year;
+    public CollectionEntityUpdateDto(int month, List<Word> wordList) {
         this.month = month;
         this.wordList = wordList;
     }
--- src/main/java/weby/kiwi/domain/collection/repository/CollectionRepository.java
+++ src/main/java/weby/kiwi/domain/collection/repository/CollectionRepository.java
@@ -3,10 +3,16 @@ package weby.kiwi.domain.collection.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import weby.kiwi.domain.collection.entity.Collection;
+import weby.kiwi.domain.word.entity.Word;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface CollectionRepository extends JpaRepository<Collection, Integer> {// pk타입으로 int지정했으나 추후 수정 필요함.
+public interface CollectionRepository extends JpaRepository<Collection, Long> {
 
     //JpaRepository 기본CRUD메서드 자동 생성
-    Collection findByUserIdAndMonth(int user_id, int month);
+    Boolean existsByCollectionAndWord(Collection collection, Word word);
+    Optional<Collection> findByUserIdAndMonth(long userId, int month);
+    List<Word> findWordList(Collection collection);
 }
